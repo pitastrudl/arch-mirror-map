a3c76df06dea7bedf5d6ea839b8e38c5457d34e4
Arun Bahl <pitastrudl@gmail.com>
2023-04-30 15:16:29 +0200

    helpers: reorg and added var checks

--- helpers.py
+++ helpers.py
@@ -1,59 +1,6 @@
 import json
 ips = ""
-# append or create to json with ip and lat/long coords
-def save_ip_loc(json_data, file_name):
-    try:
-        with open(file_name, 'w') as file:
-            json.dump(json_data, file)
-    except Exception as e:
-        raise Exception(f"Could not save file: {e}")
-
-# Loads ip location 
-def load_ip_loc(file_name):
-    try:
-        with open(file_name, 'r') as file:
-            json_data = json.load(file)
-        return json_data
-    except Exception as e:
-        raise Exception(f"Could not load file: {e}")
-
-
-def country_is_saved(country):
-    try:
-        with open('countries.json') as f:
-            data = json.load(f)
-        return country in data if data else False
-    except FileNotFoundError:
-        return False
-
-def load_saved_country(country):
-    with open('countries.json') as f:
-        data = json.load(f)
-        country = data[country]
-    return country["latitude"], country["longitude"]
-
-
-def save_country(country_name, country_data):
-    try:
-        with open('countries.json') as f:
-            countries = json.load(f)
-    except FileNotFoundError:
-        countries = {}
-
-    if country_name in countries:
-        print("Country already exists in JSON file")
-        return False
-
-    try:
-        countries[country_name] = country_data
-    except Exception as e:
-        print(f"Failed to add new country: {e}")
-        return False
-
-    with open('countries.json', 'w') as f:
-        json.dump(countries, f)
-
-    return True
+countries= ""
 
 def ip_is_saved(ip):
     global ips
@@ -98,5 +45,52 @@ def save_ip(ip, ip_data):
 
     with open('ips.json', 'w') as f:
         json.dump(ips, f)
+
+    return True
+
+def country_is_saved(country):
+    if country in countries:
+        return country in countries if countries else False
+    else:
+        try:
+            with open('countries.json') as f:
+                data = json.load(f)
+            return country in data if data else False
+        except FileNotFoundError:
+            return False
+
+def load_saved_country(country):
+    global countries
+    if countries == "":
+        with open('countries.json') as f:
+            data = json.load(f)
+            country = data[country]
+        return country["latitude"], country["longitude"]
+    else:
+        with open('countries.json') as f:
+            data = json.load(f)
+            country = data[country]
+        return country["latitude"], country["longitude"]
+
+
+def save_country(country_name, country_data):
+    global countries
+    if country_name in countries:
+        print("Country already exists in JSON file")
+        return False
+    else:
+        try:
+            with open('countries.json') as f:
+                countries = json.load(f)
+        except FileNotFoundError:
+            countries = {}
+    try:
+        countries[country_name] = country_data
+    except Exception as e:
+        print(f"Failed to add new country: {e}")
+        return False
+
+    with open('countries.json', 'w') as f:
+        json.dump(countries, f)
 
     return True
